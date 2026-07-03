5ed16de3ecd0771d9ea57c89c1fe2df4673d55d3
matos78630244 <matos78630244@gmail.com>
2026-07-02 23:58:50 -0400

    fix: revertir bootstrap app a version estable

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
-if (env('APP_ENV') === 'production') {
-    \Illuminate\Support\Facades\URL::forceScheme('https');
-}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
